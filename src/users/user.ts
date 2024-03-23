54dc42be0e2e8edff65717e8cbf03db23b289966
raacee <raceldamechli@outlook.fr>
2024-03-23 12:53:59 +0100

    removed body-parser from imports & reindent

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,6 +1,6 @@
-import bodyParser from "body-parser";
 import express from "express";
 import {BASE_USER_PORT} from "../config";
+import {Node} from "../registry/registry";
 
 export type SendMessageBody = {
   message: string;
@@ -10,27 +10,36 @@ export type SendMessageBody = {
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
-  _user.use(bodyParser.json());
 
-  let lastReceivedMessage:any = null
-  let lastSentMessage:any = null
+  let lastReceivedMessage: any = null
+  let lastSentMessage: any = null
+  let lastCircuit: Node[] = []
 
   // Implement the status route
   _user.get("/status", (req, res) => {
     res.send("live");
   });
 
-  _user.get("/getLastReceivedMessage" , (req,res) => {
+  _user.get("/getLastReceivedMessage", (req, res) => {
     res.json({"result": lastReceivedMessage})
   })
 
-  _user.get("/getLastSentMessage" , (req,res) => {
+  _user.get("/getLastSentMessage", (req, res) => {
     res.json({"result": lastSentMessage})
   })
 
-  _user.post("/sendMessage" , (req,res) => {
-    const { message, destinationUserId } = req.body
-    res.json(404)
+  _user.get("/getLastCircuit", (req, res) => {
+    res.status(200).json({result: lastCircuit.map((node) => node.nodeId)});
+  });
+
+  _user.post("/message", (req, res) => {
+    const body = req.body;
+    lastReceivedMessage = body.message;
+    res.status(200).send("success");
+  });
+
+  _user.post("/sendMessage",(req,res)=>{
+    //TODO:
   })
 
   return _user.listen(BASE_USER_PORT + userId, () => {
